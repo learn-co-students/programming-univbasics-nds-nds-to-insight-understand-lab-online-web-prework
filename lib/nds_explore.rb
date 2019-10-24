4f2ef5003d8ff80235aef245200fd5c70dddf093
Fitzhugh <j.rfitzhugh@gmail.com>
2019-10-24 16:12:42 +0000

    Done.

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -15,7 +15,7 @@ index = 0
 mov_len = directors_database[0][:movies]
 
 while index < mov_len.length do
-   name = directors_database[0][:movies][index][:title]
+   name = directors_database[0][:movies][:title]
     
    puts "#{name}\n"
   
