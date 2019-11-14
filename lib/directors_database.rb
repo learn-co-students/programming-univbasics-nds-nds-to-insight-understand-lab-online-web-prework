adbc5c083394c73a1e6f79509d88de2d4619d25d
Adrian Mohabir <mohabiradrian@gmail.com>
2019-11-14 18:31:55 +0000

    Done.

--- lib/directors_database.rb
+++ lib/directors_database.rb
@@ -1,6 +1,22 @@
-require 'yaml'
+require 'pp'
+require 'pry'
 
 def directors_database
 	rot13 = -> (s) { s.tr('A-Za-z', 'N-ZA-Mn-za-m') }
   @_db ||= YAML.load(rot13.call(File.open("directors_db").read.to_s))
 end
+
+def print_first_directors_movie_titles
+   row_index = 0 
+   binding.pry
+  while row_index < directors_database[0][:movies].length do 
+    movie = directors_database[0][:movies][row_index]
+    puts movie[:title]
+    row_index += 1 
+end 
+end
+
+
+
+
+  
