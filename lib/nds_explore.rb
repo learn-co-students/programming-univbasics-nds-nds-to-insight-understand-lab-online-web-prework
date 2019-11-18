83379d31686a009c69fb7db40ff57d26903cee13
Justin Branch <justinms@outlook.com>
2019-11-18 04:41:52 +0000

    Done.

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,6 +9,12 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
+ inner_array = 0 
+ while inner_array < directors_database[0][:movies].length do 
+    puts directors_database[0][:movies][inner_array][:title]
+    inner_array += 1
+  end
 end
 
-pp directors_database
+#pretty_print_nds(directors_database)
+#print_first_directors_movie_titles
