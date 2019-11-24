86ff8bf6d540d5195189f0717ec5a41d258323f5
dwane richards <dwanerich@gmail.com>
2019-11-24 18:35:16 +0000

    Done.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Understanding a Provided NDS
+ave # Understanding a Provided NDS
 
 ## Learning Goals
 
--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -4,9 +4,21 @@ require 'directors_database'
 
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
-  pp directors_database
+  pp nds
 end
 
 def print_first_directors_movie_titles
   
+
+  spielberg_movies = directors_database[0][:movies]
+  
+  spielberg_movies[0][:title]
+  
+  counter = 0
+  
+  while spielberg_movies.length > counter
+    puts spielberg_movies[counter][:title]
+    counter += 1
+  end
+    
 end
