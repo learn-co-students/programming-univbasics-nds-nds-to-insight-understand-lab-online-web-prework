0b52737fc95100568efc45caf5bbac16bffd95bf
Almmond Hudnall <ahudnall24@gmail.com>
2019-11-28 00:46:25 +0000

    Done.

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -1,11 +1,20 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
-# Call the method directors_database to retrieve the NDS
+vm=[:Martin_Scoressee=>"The Departed",:name=>0},{:Quentin_Tarintino=>"Hateful Eight", :name=>1}, {:DJ_Pooh=>"Friday", :name=>2}.{:Tyler_Perry=>"Acrimony", :name=>3}, {:Steven_Spilnerg=>"The Color Purple", :price=>4},
 
 def pretty_print_nds(nds)
-  # Change the code below to pretty print the nds with pp
+    
+      {:Martin_Scoressee=>"The Departed", :name=>0}, # Hashes with two keys!
+      {:Quentin_Tarintino=>"Hateful Eight", :name=>1}, # and another Hash
+      {:DJ_Pooh=>"Friday", :name=>2}, # and another Hash
+      
+    ], # end of inner array
+    [ #...and so on...
+      {:Tyler_Perry=>"Acrimony", :name=>3},
+     {:Steven_Spilnerg=>"The Color Purple", :price=>4},# Change the code below to pretty print the nds with pp
   nil
 end
 
 def print_first_directors_movie_titles
 end
+
