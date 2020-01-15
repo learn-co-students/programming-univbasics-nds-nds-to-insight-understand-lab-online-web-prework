d33376509126b7a9c665631d46f97014b61c5e4b
Khalid T <khalid.tissire@gmail.com>
2020-01-15 18:17:45 +0000

    Done.

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,10 +9,10 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-  row_index = 0
+  i = 0
   arr = directors_database[0][:movies]
-  while row_index < arr.length
-    puts arr[row_index][:title]
-    row_index += 1
+  while i < arr.length
+    puts arr[i][:title]
+    i += 1
   end
 end
